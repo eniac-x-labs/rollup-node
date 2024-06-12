c410c48861af9585ec3df6039f039aed0c211464
wwqicode-blkchain <wangwenqiliu@gmail.com>
2024-06-12 20:01:52 +0800

    feat(envConfig): add env file for anytrust eigenda and nearda to set config with env flags

--- x/anytrust/config.go
+++ x/anytrust/config.go
@@ -1,24 +1,24 @@
 package anytrust
 
 type AnytrustConfig struct {
-	RpcUrl            string `toml:"rpcUrl"`
-	RestfulUrl        string `toml:"restfulUrl"`
-	DataRetentionTime uint64 `toml:"dataRetentionTime"`
+	RpcUrl            string `toml:"rpc_url" mapstructure:"rpc_url"`
+	RestfulUrl        string `toml:"restful_url" mapstructure:"restful_url"`
+	DataRetentionTime uint64 `toml:"data_retention_time" mapstructure:"data_retention_time"`
 
-	RandomMessageSize int `toml:"randomMessageSize"`
+	RandomMessageSize int `toml:"random_message_size" mapstructure:"random_message_size"`
 	//DASRetentionPeriod time.Duration `toml:"dasRetentionPeriod"`
-	SigningKey string `toml:"signingKey"`
+	SigningKey string `toml:"signing_key" mapstructure:"signing_key"`
 	//SigningWallet         string        `toml:"signingWallet"`
 	//SigningWalletPassword string        `toml:"signingWalletPassword"`
 }
 
 const (
-	RpcUrlFlag             = "rpcUrl"
-	RestfulUrlFlag         = "restfulUrl"
-	DataRetentionTimeFlag  = "dataRetentionTime"
-	RandomMessageSizeFlag  = "randomMessageSize"
-	DasRetentionPeriodFlag = "dasRetentionPeriod"
-	SigningKeyFlag         = "signingKey"
+	RpcUrlFlag            = "rpc_url"
+	RestfulUrlFlag        = "restful_url"
+	DataRetentionTimeFlag = "data_retention_time"
+	RandomMessageSizeFlag = "random_message_size"
+	//DasRetentionPeriodFlag = "dasRetentionPeriod"
+	SigningKeyFlag = "signing_key"
 )
 
 // AnytrustDAEnvFlags The env flag is like prefix_flag, with all letters in uppercase.
@@ -27,6 +27,6 @@ var AnytrustDAEnvFlags = []string{
 	RestfulUrlFlag,
 	DataRetentionTimeFlag,
 	RandomMessageSizeFlag,
-	DasRetentionPeriodFlag,
+	//DasRetentionPeriodFlag,
 	SigningKeyFlag,
 }
